819991679df121a60b77f8ef57bbb42d7af6ef55
Eric Fowler <ericfowler303@gmail.com>
2015-02-02 12:26:53 -0700

    Initial Commit
    includes tests

---
+++ Disemvoweler/Disemvoweler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disemvoweler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // keep the console open
+            Console.ReadKey();
+        }
+        public static string Disemvoweler(string input)
+        {
+            
+            // Write out the various string results
+            Console.WriteLine("Original: " );
+            Console.WriteLine("Disemvoweled: " );
+            Console.WriteLine("Vowels Removed: " ); 
+            // Return the Disemvoweled string as well for testing
+            return string.Empty;
+        }
+    }
+}
---
+++ Disemvoweler/Disemvoweler/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Disemvoweler
+{
+    [TestFixture]
+    class Test
+    {
+        [Test,Timeout(500)]
+        public void testExample1()
+        {
+            Assert.IsNotNullOrEmpty(Program.Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!"));
+            StringAssert.AreEqualIgnoringCase("NcklbcksmyfvrtbndThrsngwrtngcntbbt", Program.Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!"));
+        }
+        [Test, Timeout(500)]
+        public void testExample2()
+        {
+            Assert.IsNotNullOrEmpty(Program.Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?"));
+            StringAssert.AreEqualIgnoringCase("Hwmnybrscldbrgryllsgrllfbrgryllscldgrllbrs", Program.Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?"));
+        }
+        [Test, Timeout(500)]
+        public void testExample3()
+        {
+            Assert.IsNotNullOrEmpty(Program.Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls."));
+            StringAssert.AreEqualIgnoringCase("mcdnnjbbymkthfnctnsndmkthclls", Program.Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls."));
+        }
+    }
+}
